43de7c45ec3b6ca8c4e69b88ffa58bb264809938
Quan Trong Anh Huy <huyqta@live.com>
2017-09-07 17:33:21 +0700

    Implement Shop Online project

---
+++ ShopOnline/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Models;
+using EntityModel;
+
+namespace ShopOnline.Controllers
+{
+    public class HomeController : Controller
+    {
+        GeneralContext _context;
+
+        public HomeController(GeneralContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            HomeViewModel model = new HomeViewModel();
+            model.ListCategory = _context.Category.Where(cat => cat.Id != -1).ToList();
+            model.ListProduct = _context.Product.ToList();
+            return View(model);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ShopOnline/Models/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using EntityModel;
+using EntityModel.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopOnline.Models
+{
+    public class HomeViewModel
+    {
+        public List<Category> ListCategory { get; set; }
+
+        public List<Product> ListProduct { get; set; }
+    }
+}
