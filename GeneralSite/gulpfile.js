b251ed92c0c291cbc2b867e6109c9a325177303b
Quan Trong Anh Huy <huyqta@live.com>
2017-09-19 00:31:31 +0700

    add scss general site

---
+++ GeneralSite/gulpfile.js
@@ -0,0 +1,16 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    fs = require("fs"),
+    less = require("gulp-less"),
+    sass = require("gulp-sass");
+
+gulp.task('default', function () {
+    // place code for your default task here
+    return gulp.src('wwwroot/css/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('wwwroot/css'));
+});
