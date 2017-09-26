38887845ad9324a0d901e68d8fd36c4933bca5ed
Quan Trong Anh Huy <anhhuydng@gmail.com>
2017-09-26 15:15:05 +0700

    Fix some bug Add language pack

---
+++ LanguagePack/LanguageManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+
+namespace LanguagePack
+{
+    public class LanguageManager
+    {
+        public static string GetCommonUrl(string name)
+        {
+            var resourceManager = new ResourceManager(typeof(CommonUrls));
+
+            try
+            {
+                string returnValue = resourceManager.GetString(name, CultureInfo.CurrentCulture);
+
+                if (!string.IsNullOrEmpty(returnValue))
+                {
+                    return returnValue;
+                }
+            }
+            catch (Exception)
+            {
+                return "Resource Undefined";
+            }
+
+            return "Resource Undefined";
+        }
+
+        public static string GetLabel(string name)
+        {
+            var resourceManager = new ResourceManager(typeof(Labels));
+            try
+            {
+                string returnValue = resourceManager.GetString(name, CultureInfo.CurrentCulture);
+
+                if (!string.IsNullOrEmpty(returnValue))
+                {
+                    return returnValue;
+                }
+            }
+            catch (Exception)
+            {
+                return "Resource Undefined";
+            }
+
+            return "Resource Undefined";
+        }
+
+        public static string GetMessage(string name)
+        {
+            var resourceManager = new ResourceManager(typeof(Messages));
+
+            try
+            {
+                string returnValue = resourceManager.GetString(name, CultureInfo.CurrentCulture);
+
+                if (!string.IsNullOrEmpty(returnValue))
+                {
+                    return returnValue;
+                }
+            }
+            catch (Exception)
+            {
+                return "Resource Undefined";
+            }
+
+            return "Resource Undefined";
+        }
+    }
+}
